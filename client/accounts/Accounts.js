955860de537bb9c7f435e6aa5e2169269dec6130
Weiming Zhai <wmzhai@gmail.com>
2016-03-25 13:08:04 +0800

    fix server side account bug

--- client/accounts/Accounts.js
+++ client/accounts/Accounts.js
@@ -1,10 +1,34 @@
-
-var postSignUp = function ( userId, info ) {
-  console.log(userId);
-  console.log(info);
+var myLogoutFunc = function () {
+  Session.set('nav-toggle', '');
+  FlowRouter.go('/');
 };
 
 AccountsTemplates.configure({
-  postSignUpHook: postSignUp
+  confirmPassword: false,
+  onLogoutHook: myLogoutFunc
 });
 
+AccountsTemplates.addFields([
+  {
+    _id: 'firstName',
+    type: 'text',
+    displayName: 'First Name',
+    required: true
+  },{
+    _id: 'profession',
+    type: 'select',
+    displayName: 'Profession',
+    select: [
+      {
+        text: 'Developer',
+        value: 'developer'
+      }, {
+        text: 'Designer',
+        value: 'designer'
+      },{
+        text: 'Other',
+        value: 'other'
+      }
+    ]
+  }
+]);
---
+++ server/Accounts.js
@@ -0,0 +1,10 @@
+
+var postSignUp = function ( userId, info ) {
+  console.log(userId);
+  console.log(info);
+};
+
+AccountsTemplates.configure({
+  postSignUpHook: postSignUp
+});
+
