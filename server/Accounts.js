9693718b94cf1d618dd06df27a4ee084c9d9cc9a
Weiming Zhai <wmzhai@gmail.com>
2016-03-25 13:47:43 +0800

    roles

--- server/Accounts.js
+++ server/Accounts.js
@@ -1,7 +1,8 @@
 
 var postSignUp = function ( userId, info ) {
   console.log(userId);
-  console.log(info);
+  console.log(info.profile.profession);
+  Roles.addUsersToRoles(userId, ['normal-user', info.profile.profession], 'example.com');
 };
 
 AccountsTemplates.configure({
