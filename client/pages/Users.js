658ca0479f3ccf5f206f47c30924c3723a5bf601
Weiming Zhai <wmzhai@gmail.com>
2016-03-25 15:50:55 +0800

    display user list

--- client/pages/Users.js
+++ client/pages/Users.js
@@ -3,3 +3,15 @@ Template.Users.onCreated(function () {
     this.subscribe('allUsers');
   });
 });
+
+Template.Users.helpers({
+  users: function () {
+   return Meteor.users.find();
+  },
+  userEmail: function () {
+    return this.emails[0].address;
+  },
+  isAdmin: function () {
+    return Roles.userIsInRole(this._id, 'admin')? 'admin': 'normal';
+  }
+});
